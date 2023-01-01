e95717039a5b8c40cc578b6cfefce96784d50106
freeleeman <62164480+freeleeman@users.noreply.github.com>
2023-01-01 18:09:13 +0800

    Update diy5.4.sh

--- diy5.4.sh
+++ diy5.4.sh
@@ -12,18 +12,18 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
+#sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 
 # mv -vf ../mt7621_phicomm_k2p.dts ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 
 git clone https://github.com/fw876/helloworld package/helloworld
-git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
+#git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
 git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
-git clone https://github.com/freeleeman/luci-app-dockerman.git package/luci-app-dockerman
+#git clone https://github.com/freeleeman/luci-app-dockerman.git package/luci-app-dockerman
 git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
@@ -32,11 +32,11 @@ git clone https://github.com/freeleeman/luci-theme-opentomcat.git package/luci-t
 git clone https://github.com/freeleeman/luci-theme-opentomato.git package/luci-theme-opentomato
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+#git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 #git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
 #git clone https://github.com/ledewrt/luci-app-ledeproxy.git package/luci-app-ledeproxy
 #git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
-git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
-git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git package/luci-app-UUGameAcc
+#git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git package/luci-app-UUGameAcc
 #git clone https://github.com/freeleeman/luci-app-dockerman.git package/luci-app-dockerman
+
